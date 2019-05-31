c0460c459447475dcd2ab5b1a015afec00923357
anmolbansal <bansalanmol482@gmail.com>
2019-05-31 23:06:56 +0530

    Test comment

--- app/src/main/java/com/example/web_view/MainActivity.java
+++ app/src/main/java/com/example/web_view/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //This is my test code
+        //This is my test code for anmol bansal.
     }
 }
